a0d829312afea7937f0d65592a7465800de8c087
krrishchawla <krrishchawla1@gmail.com>
2023-11-05 01:00:48 -0800

    llm models

--- llm.py
+++ llm.py
@@ -2,8 +2,11 @@ import openai
 from tenacity import retry, stop_after_attempt, wait_random_exponential
 import os
 
+GPT3 = "gpt-3.5-turbo"
+GPT4 = "gpt-4"
+
 class GPT4QAModel():
-    def __init__(self, model="gpt-4"):
+    def __init__(self, model=GPT4):
         openai.api_key = os.environ.get('OPENAI_API_KEY')
         self.model = model
 
@@ -21,8 +24,8 @@ class GPT4QAModel():
         return response["choices"][0]['message']['content'].strip()
 
 
-    @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6))
-    def answer_question(self, question, max_tokens=2000, stop_sequence=None):
+    @retry(wait=wait_random_exponential(min=1, max=40), stop=stop_after_attempt(6))
+    def answer_question(self, question, max_tokens=3000, stop_sequence=None):
 
         try:
             return self. _attempt_answer_question(question, max_tokens=max_tokens, stop_sequence=stop_sequence)
